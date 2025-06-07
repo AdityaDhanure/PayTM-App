3685b090ec71a2c0c841b3f940171b43f7d51534
AdityaDhanure <adityadhanure0607@gmail.com>
2025-06-07 21:44:37 +0530

    final commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,12 +5,12 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = require("../config");
 const {authMiddleware} = require("../middleware");
 
-const cors = require("cors");
+// const cors = require("cors");
 
-app.use(cors({
-  origin: "https://paytm-app-8h79.onrender.com/api/v1/user", // or "*" for all
-  methods: ["GET", "POST", "PUT", "DELETE"]
-}));
+// app.use(cors({
+//   origin: "https://paytm-app-8h79.onrender.com/api/v1/user", // or "*" for all
+//   methods: ["GET", "POST", "PUT", "DELETE"]
+// }));
 
 
 const router = express.Router();
